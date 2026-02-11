343deaa58118499792c6d305dc8381f8cc5b7998
promani-webs <promani.webs@gmail.com>
2026-02-11 22:02:31 +0100

    Update runner.go

--- runner/runner.go
+++ runner/runner.go
@@ -295,9 +295,5 @@ func banner(messages []string, width int) string {
 }
 
 func Banner() {
-	message1 := "🌍 Google Maps Scraper"
-	message2 := "⭐ If you find this project useful, please star it on GitHub: https://github.com/gosom/google-maps-scraper"
-	message3 := "💖 Consider sponsoring to support development: https://github.com/sponsors/gosom"
-
-	fmt.Fprintln(os.Stderr, banner([]string{message1, message2, message3}, 0))
+	// empty
 }
